9f9761c4d5c8a407a8470d64882fd9378ae93cba
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-03-26 12:55:33 -0700

    Add a README

--- src/components/term.js
+++ src/components/term.js
@@ -44,19 +44,21 @@ class Term extends React.Component {
 This is an interactive demonstration of some of the netlify CLI features.
 
 You'll see help messages in __a different color__ and you can click on any
-highlighted block of text to copy those words to the prompt.
+__**highlighted block**__ to copy those words to the prompt.
 
 If you go through the whole tutorial, you'll learn how to:
 
-* Do a manual deploy of a folder
-* Managing environments
-* Settings up continuous deployment
-* Naming your site
-* Setting up a custom domain
+* Do a manual deploy of a site folder
+* Manage environments
+* Password protect your staging site
+* Set up continuous deployment
+* Name your site
+* Configure a custom domain
 
 ## Run into a bug?
 
-Why not open and issue or fork this tutorial and send us a pull request:
+Great! Help us squash it! Open an issue or fork this tutorial and send us a
+pull request:
 
 https://github.com/netlify/netlify-cli-tutorial
 
