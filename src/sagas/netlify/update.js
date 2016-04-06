5c9b061c3d6fed99211c85dec4f1adac0bb51527
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-04-06 10:22:58 -0700

    A bit more help text

--- src/sagas/netlify/update.js
+++ src/sagas/netlify/update.js
@@ -13,7 +13,7 @@ export function* update(args) {
     '',
     '__Sadly this is as far as the netlify tutorial goes at this point__',
     '__To continue your journey, install the real CLI from your terminal__',
-    '__Or visit [[https://app.netlify.com]] to login to the Web UI__',
+    '__Or visit [[https://app.netlify.com]] to login to the Web UI and view your new site there__',
     ''
   ));
 }
